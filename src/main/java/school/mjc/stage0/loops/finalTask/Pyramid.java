49f93d9cb27bb843abb6e8405cefbb257f37043e
Muslimbek Soliyev <abuabdulloh0709@gmail.com>
2023-01-12 11:54:26 +0500

    module4 final task loops

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,6 +3,18 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
 
+        String x = "";
+        for (int i = 1; i <= cathetusLength; i++) {
+            x = "1";
+            for (int j = 2; j <= cathetusLength; j++) {
+                if (j <= i) {
+                    x = j + x + j;
+                } else {
+                    x=" "+x;
+                }
+            }
+            System.out.println(x);
+        }
     }
 
     public static void main(String[] args) {
